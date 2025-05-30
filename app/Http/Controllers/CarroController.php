78c6cef7c8dac7fbf87a97b8d872371cdee9511b
olga <olgazr777@gmail.com>
2025-05-30 11:29:30 -0300

    initial commit

---
+++ app/Http/Controllers/CarroController.php
@@ -0,0 +1,28 @@
+namespace App\Http\Controllers;
+
+use App\Models\Carro;
+use Illuminate\Http\Request;
+
+class CarroController extends Controller
+{
+    // Listar todos os carros
+    public function index()
+    {
+        return Carro::all();
+    }
+
+    // Cadastrar um novo carro
+    public function store(Request $request)
+    {
+        $request->validate([
+            'marca' => 'required|string',
+            'modelo' => 'required|string',
+            'ano' => 'required|integer',
+            'preco' => 'required|numeric',
+        ]);
+
+        $carro = Carro::create($request->all());
+
+        return response()->json($carro, 201);
+    }
+}
